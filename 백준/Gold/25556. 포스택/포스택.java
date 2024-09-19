e8a2153228dc65efc13f74d11af0fe3e85c378aa
jiheaBaik <105536725+jiheaBaik@users.noreply.github.com>
2024-09-19 16:36:44 +0900

    [Gold V] Title: 포스택, Time: 900 ms, Memory: 115284 KB -BaekjoonHub

---
+++ 백준/Gold/25556. 포스택/포스택.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        List<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+           nums.add(sc.nextInt());
+            //nums.add(Integer.parseInt(sc.next()));
+        }
+
+        List<Stack<Integer>> stList = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            stList.add(new Stack<>());
+        }
+
+        String result = "NO";
+
+        for (int num : nums) {
+            result = "NO";
+            for (Stack<Integer> integers : stList) {
+                if (integers.isEmpty() || integers.peek() < num ) {
+                    integers.push(num);
+                    result = "YES";
+                    break;
+                }
+            }
+
+            //stack을 다 돌았는데도 NO라면
+            if (result.equals("NO")) {
+                break;
+            }
+        }
+
+        System.out.println(result);
+        
+    }
+}
